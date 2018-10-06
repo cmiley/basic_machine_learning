36224a6003810991e9a0b0cd74be0ff1ca26aee2
cmiley <cmiley@nevada.unr.edu>
2018-10-06 09:52:21 -0700

    Added more to perceptron.

--- cayler_miley_project2_691/perceptron.py
+++ cayler_miley_project2_691/perceptron.py
@@ -1,11 +1,17 @@
 import numpy as np
 
+X1 = np.array([[0, 1], [1, 0], [5, 4], [1, 1], [3, 3], [2, 4], [1, 6]])
+Y1 = np.array([[1], [1], [-1], [1], [-1], [-1], [-1]])
+
+X2 = np.array([[-2, 1], [1, 1], [1.5, -0.5], [-2, 1], [-1, -1.5], [2, -2]])
+Y2 = np.array([[1], [1], [1], [-1], [-1], [-1]])
+
 
 def update_weights(sample, label, weights, bias, FLAG_debug=False):
     activation = np.dot(sample, weights) + bias
-    if activation*label <= 0:
+    if activation * label <= 0:
         for index, weight in enumerate(weights):
-            weights[index] = weight + sample[index]*label
+            weights[index] = weight + sample[index] * label
 
         bias = bias + label
 
@@ -15,16 +21,34 @@ def update_weights(sample, label, weights, bias, FLAG_debug=False):
     return weights, bias
 
 
+def converged(w_stale, b_stale, w, b, epoch, max_epochs):
+    if np.any(w == 0) and b == 0:
+        return False
+
+    if np.array_equal(w_stale, w) and b_stale == b:
+        return True
+    elif epoch >= max_epochs:
+        return True
+
+    return False
+
+
 def perceptron_train(X, Y, FLAG_debug=False):
     # initialize weights and bias
     w = np.zeros(X[0].size)
     b = 0
+    epoch = 0
+    max_epochs = 1000
 
-    for index, sample in enumerate(X):
-        w, b = update_weights(sample, Y[index], w, b)
+    w_stale = np.zeros(X[0].size)
+    b_stale = 0
+
+    while not converged(w_stale, b_stale, w, b, epoch, max_epochs):
+        for index, sample in enumerate(X):
+            w, b = update_weights(sample, Y[index], w, b)
 
     if FLAG_debug:
-        print("Debuggin...")
+        print("Debugging...")
         print("W: {}, B: {}".format(w, b))
 
     return w, b
@@ -35,10 +59,10 @@ def perceptron_test(X_test, Y_test, w, b, FLAG_debug=False):
 
     for index, sample in enumerate(X_test):
         activation = np.dot(sample, w) + b
-        if activation*Y_test[index] > 0:
+        if activation * Y_test[index] > 0:
             correct += 1
 
-    accuracy = correct/X_test.size
+    accuracy = correct / X_test.size
 
     if FLAG_debug:
         print("Testing accuracy: {}".format(accuracy))
@@ -47,7 +71,8 @@ def perceptron_test(X_test, Y_test, w, b, FLAG_debug=False):
 
 
 def main():
-    pass
+    w, b = perceptron_train(X1, Y1, True)
+    acc = perceptron_test(X1, Y1, w, b, True)
 
 
 if __name__ == "__main__":
