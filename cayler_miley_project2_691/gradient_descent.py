9e607d504a901037a2b938b6b701b57e39ad9240
cmiley <cmiley@nevada.unr.edu>
2018-10-15 12:56:54 -0700

    Completed gradient descent.

--- cayler_miley_project2_691/gradient_descent.py
+++ cayler_miley_project2_691/gradient_descent.py
@@ -1,18 +1,40 @@
 import numpy as np
+import math
 
 
 def gradient_descent(grad, x_init, eta):
     threshold = 1e-5
 
     minimum = x_init
-    while grad(minimum) > threshold:
-        minimum = minimum - eta*grad(minimum)
+    while np.linalg.norm(grad(minimum)) > threshold:
+        minimum = np.subtract(minimum, np.multiply(eta, grad(minimum)))
 
     return minimum
 
 
+# comment
+
+
+def df_squared(x):
+    return np.array([2 * x[0]])
+
+
+def best_grad(x):
+    a, b = 10, 7
+    return np.array([2.0 * (x[0] - a), 2.0 * (x[1] - b)])
+
+
+def df(x):
+    return np.array([4 * math.pow(x[0], 3), 6 * x[1]])
+
+
 def main():
-    pass
+    x = gradient_descent(df_squared, np.array([5.0]), 0.1)
+    print("minimum occurs at: {}".format(x))
+    x2 = gradient_descent(df, np.array([1.0, 1.0]), 0.01)
+    print("minimum occurs at: {}".format(x2))
+    x3 = gradient_descent(best_grad, np.zeros(2), 0.01)
+    print("minimum occurs at: {}".format(x3))
 
 
 if __name__ == "__main__":
